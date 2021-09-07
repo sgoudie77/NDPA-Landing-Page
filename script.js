508a91edc26b41591bbe6f4aa4ef93a0c0267e86
Sean Goudie <sgoudie77@hotmail.com>
2021-09-07 10:55:50 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// responsive toggle
+function toggleMenu() {
+    const toggleMenu = document.querySelector('.toggleMenu');
+    const navbar = document.querySelector('.navbar');
+    toggleMenu.classList.toggle('active');
+    navbar.classList.toggle('active');
+}
